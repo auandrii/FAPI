3a22fce736b8c16f885a35259626a1e04af051c5
andrii <mag220i380@gmail.com>
2024-04-08 09:17:44 +0300

    Modifi

--- crawler.py
+++ crawler.py
@@ -11,34 +11,24 @@ async def crawl_and_extract_text(url):
     """
     Use Playwright to crawl the given URL and extract text.
     """
-
     async with async_playwright() as p:
-        for browser_type in [p.chromium, p.firefox, p.webkit]:
-            browser = await browser_type.launch()
-            page = await browser.new_page()
-            await page.goto(url)
-            #await page.screenshot(path=f'example-{browser_type.name}.png')
-            # print(f' @@@@@@@@@@@@@@@@@')
-            # browser = await p.chromium.launch(proxy=proxy)
-            # print(f' *****************88')
-            # page = await browser.new_page()
-            # await page.goto(url)
-            # # Extract main page text
-            text = await page.text_content('body')
-            #
-            # # Additional URLs to crawl
-            additional_urls = [url + "/contact", url + "/about"]
-            for additional_url in additional_urls:
-                 try:
-                     await page.goto(additional_url)
-                     text += "\n" + await page.text_content('body')
+        browser = await p.chromium.launch(proxy=proxy)
+        page = await browser.new_page()
+        await page.goto(url)
+        # Extract main page text
+        text = await page.text_content('body')
 
-                 except Exception as e:
-                     print(f"Error crawling {additional_url}: {str(e)}")
-            #
-            print(text)
-            await browser.close()
-            #
-            # # Process and clean the text
-            clean_text = re.sub(r'\s+', ' ', text).strip()
-            return clean_text
+        # Additional URLs to crawl
+        additional_urls = [url + "/contact", url + "/about"]
+        for additional_url in additional_urls:
+            try:
+                await page.goto(additional_url)
+                text += "\n" + await page.text_content('body')
+            except Exception as e:
+                print(f"Error crawling {additional_url}: {str(e)}")
+
+        await browser.close()
+
+        # Process and clean the text
+        clean_text = re.sub(r'\s+', ' ', text).strip()
+        return clean_text
